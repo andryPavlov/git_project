47f5e4e6865af6031c3009126a6d992cad301feb
Nikolai Korolev <nikolaykorolev.000@gmail.com>
2023-12-07 02:38:54 +0300

    Это рабочая версия программы сложения

---
+++ function.py
@@ -0,0 +1,9 @@
+def summ(a: int, b: int) -> int:
+    '''
+    Возвращает значения суммы а и b
+    :param a: 2
+    :param b: 4
+    :return: 6
+    '''
+
+    return a + b
--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print('Hello Git')
+from function import summ as s
+
+
+def main():
+    '''
+    Точка входа программы
+    :return: None
+    '''
+
+    try:
+        a, b = map(int, input('Введите значения а и б разделя пробелом: ').split())
+    except Exception as e:
+        print(f'Твоя ошбка - {e}')
+    finally:
+        print(s(a, b))
+
+
+if __name__ == '__main__':
+    main()
